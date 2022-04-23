6d9bfa435800d76672d285ac292d574ab96eefdd
Murilo Ferreira <murilo.ferreira32@outlook.com>
2022-04-22 23:53:43 -0300

    Adicionando Classes
    Classes, herança, metodos.

---
+++ Classes/Classes.cs
@@ -0,0 +1,17 @@
+namespace Classes
+{
+Classes e Objetos
+
+Classes são os tipos mais fundamentais em C#.
+Uma classe é uma estrutura de dados que combina estado (campos) e  ações (métodos).
+
+Objetos são instancias de uma classe.
+As classes suportam herança e polimorfismo, mecanismos pelos quais as classes derivadas podem estender e especializar a classe base.
+
+
+Instâncias de classes (objetos) são criadas usando o operador new, que aloca memória para umanova instância, chama um construtor para inicializar a instância e retorna uma referência a instância
+
+Ponto p1 = new Ponto (0,0);
+Ponto p2 = new Ponto(10,20);
+
+}
---
+++ Classes/Herança/Herança.cs
@@ -0,0 +1,16 @@
+using System;
+using Classes.herança
+
+    namespace Classes
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Ponto p1 = new Ponto(20, 30);
+
+                Ponto p2 = new Ponto(10, 20, 30);
+
+            }
+        }
+    }
